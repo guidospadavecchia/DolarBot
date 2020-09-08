89a0a12580eee3de5fbe5b8cad3989dac34e133d
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2020-09-08 00:57:55 -0300

    Create HelpModule.cs

---
+++ src/SteamBuddy.Modules/Commands/HelpModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace SteamBuddy.Modules.Commands
+{
+    public class HelpModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        [Summary("Sends a DM with the bot's command list.")]
+        public async Task SendHelp(int help1, int help2)
+        {
+            //TODO
+        }
+    }
+}

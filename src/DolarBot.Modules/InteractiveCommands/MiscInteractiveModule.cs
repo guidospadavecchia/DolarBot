78434a24ebd9b48f6f685c2840756ae9047fbc85
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2022-03-18 22:27:40 -0300

    Update MiscInteractiveModule.cs

--- src/DolarBot.Modules/InteractiveCommands/MiscInteractiveModule.cs
+++ src/DolarBot.Modules/InteractiveCommands/MiscInteractiveModule.cs
@@ -19,7 +19,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DolarBot.Modules.Commands
+namespace DolarBot.Modules.InteractiveCommands
 {
     /// <summary>
     /// Contains misc commands.

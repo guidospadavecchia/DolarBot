420b0da8c9d2bb4ed902020dcf843e13f55848dd
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2022-04-03 19:50:49 -0300

    Migrate RealModule to slash commands

---
+++ src/DolarBot.Modules/InteractiveCommands/Choices/RealChoices.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Interactions;
+
+namespace DolarBot.Modules.InteractiveCommands.Choices
+{
+    /// <summary>
+    /// The available choices for the dollar command.
+    /// </summary>
+    public enum RealChoices
+    {
+        [ChoiceDisplay("Oficial")]
+        Oficial,
+        [ChoiceDisplay("Ahorro")]
+        Ahorro,
+        [ChoiceDisplay("Blue")]
+        Blue,
+    }
+}
